09545f06af1934f1430ba0f69e4515adb089db20
Ewa Olczak <ewa.olczak@gmail.com>
2023-02-18 16:22:18 +0100

    DONE RWD Banner, ADD media to TopBar

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -10,24 +10,24 @@ const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className={`row ${styles.banner}`}>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
-        <div className={`col text-center ${styles.logo}`}>
+        <div className={`col-6 col-sm text-sm-left order-sm-1 ${styles.logo}`}>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
         </div>
-        <div className={`col text-right ${styles.cart}`}>
+        <div className={`col-6 col-sm text-right order-sm-2 ${styles.cart}`}>
           <a href='#' className={styles.cartBox}>
             <div className={styles.cartIcon}>
               <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
             </div>
             <div className={styles.cartCounter}>0</div>
           </a>
+        </div>
+        <div className={`col text-left order-sm-0 ${styles.phoneNumber}`}>
+          <p>
+            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
+            222
+          </p>
         </div>
       </div>
     </div>
