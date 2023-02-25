5c5c2d69e94bce56bf24692c2e1f589863f3b5b4
Ewa Olczak <ewa.olczak@gmail.com>
2023-02-25 18:44:54 +0100

    Add PropTypes and fix brandsReducer

--- src/components/common/BrandBox/BrandBox.js
+++ src/components/common/BrandBox/BrandBox.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './BrandBox.module.scss';
 
-// const BrandBox = ({ image }) => {
-//   return (
-//     <div className={styles.root}>
-//       <div className={styles.logoBox}>
-//         <div className={styles.brandLogo}>
-//           <img src={image} alt='brand logo' />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const BrandBox = ({ image }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.logoBox}>
+        <img className={styles.brandLogo} src={image} alt='brand logo' />
+      </div>
+    </div>
+  );
+};
+BrandBox.propTypes = {
+  image: PropTypes.string,
+};
 
-// export default BrandBox;
+export default BrandBox;
