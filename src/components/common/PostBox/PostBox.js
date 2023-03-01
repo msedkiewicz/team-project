8d22274215c2b14ca11a1c9496256d3c6624299b
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-01 23:43:01 +0100

    Add BlogSection with styles

---
+++ src/components/common/PostBox/PostBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './PostBox.module.scss';
+
+const PostBox = () => {
+  return (
+    <div className={styles.root}>
+      <h1>dskglhfglik</h1>
+    </div>
+  );
+};
+
+export default PostBox;
