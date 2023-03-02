ca7698ce12c774e9652bbc558a8befb2cf1f9122
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-02 22:36:58 +0100

    Add ProductOverview files

---
+++ src/components/common/ProductOverview/ProductOverview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ProductOverview.module.scss';
+
+const ProductOverview = () => {
+  return (
+    <div className={styles.root}>
+      <h1>ProductOverview</h1>
+    </div>
+  );
+};
+
+export default ProductOverview;
