36b7cc24a5b49c90185f4444c2876a231149a72d
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-03 00:01:18 +0100

    IN PROGRESS

--- src/components/common/ProductOverview/ProductOverview.js
+++ src/components/common/ProductOverview/ProductOverview.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ProductOverview.module.scss';
 
-const ProductOverview = () => {
+const ProductOverview = ({ product }) => {
+  console.log('product', product);
+
   return (
     <div className={styles.root}>
-      <h1>ProductOverview</h1>
+      <div>
+        <h1>{product.name}</h1>
+      </div>
     </div>
   );
+};
+
+ProductOverview.propTypes = {
+  product: PropTypes.object,
 };
 
 export default ProductOverview;
--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import ProductOverview from '../../common/ProductOverview/ProductOverview';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}></div>;
+const ProductPage = () => (
+  <div className={styles.root}>
+    <ProductOverview />
+    {/* <h1>skfhdklfd</h1> */}
+  </div>
+);
 
 // ProductPage.propTypes = {};
 
