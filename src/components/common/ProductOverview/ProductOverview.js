0ca16736e24831e7570750a434640a8ec3d64f7a
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-03 23:29:11 +0100

    Add left part of ProductOverview

--- src/components/common/ProductOverview/ProductOverview.js
+++ src/components/common/ProductOverview/ProductOverview.js
@@ -1,21 +1,75 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './ProductOverview.module.scss';
+import Button from '../Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faUpRightAndDownLeftFromCenter,
+} from '@fortawesome/free-solid-svg-icons';
 
-const ProductOverview = ({ product }) => {
-  console.log('product', product);
-
+const ProductOverview = () => {
   return (
     <div className={styles.root}>
-      <div>
-        <h1>{product.name}</h1>
+      <div className={styles.greyBg}></div>
+      <div className='container'>
+        <div className={styles.productOverviewContainer}>
+          <div className={`col-5`}>
+            <div className={styles.photosContainer}>
+              <div className={styles.mainPhoto}>
+                <img
+                  src='/images/furniture_images/bed_images/bed-2.jpg'
+                  alt='mebelek'
+                />
+                <Button className={styles.buttonZoom} variant='outline'>
+                  <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
+                </Button>
+              </div>
+              <div className={styles.bottomImagesContainer}>
+                <div className={styles.smallImages}>
+                  <div className={styles.smallImage}>
+                    <img
+                      src='/images/furniture_images/bed_images/bed-3.jpg'
+                      alt='mebelek'
+                    />
+                  </div>
+                  <div className={styles.smallImage}>
+                    <img
+                      src='/images/furniture_images/bed_images/bed-3.jpg'
+                      alt='mebelek'
+                    />
+                  </div>
+                  <div className={styles.smallImage}>
+                    <img
+                      src='/images/furniture_images/bed_images/bed-3.jpg'
+                      alt='mebelek'
+                    />
+                  </div>
+                  <div className={styles.smallImage}>
+                    <img
+                      src='/images/furniture_images/bed_images/bed-3.jpg'
+                      alt='mebelek'
+                    />
+                  </div>
+                </div>
+                <div className={styles.buttonsLeftRight}>
+                  <Button className={styles.buttonLeft} variant='outline'>
+                    <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+                  </Button>
+                  <Button className={styles.buttonRight} variant='outline'>
+                    <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`col-7`}>
+            <h5>name of product</h5>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
-
-ProductOverview.propTypes = {
-  product: PropTypes.object,
 };
 
 export default ProductOverview;
--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -5,6 +5,7 @@ import styles from './ProductPage.module.scss';
 
 const ProductPage = () => (
   <div className={styles.root}>
+    <div className={styles.greyBg}></div>
     <ProductOverview />
     {/* <h1>skfhdklfd</h1> */}
   </div>
