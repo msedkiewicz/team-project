b98abb5ebc587075071d9fc25cdc307638e9392a
Ewa Olczak <ewa.olczak@gmail.com>
2023-03-04 00:49:13 +0100

    Add brands font awesome and part of right part of component

--- src/components/common/ProductOverview/ProductOverview.js
+++ src/components/common/ProductOverview/ProductOverview.js
@@ -6,7 +6,21 @@ import {
   faAngleLeft,
   faAngleRight,
   faUpRightAndDownLeftFromCenter,
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+  faMinus,
+  faPlus,
 } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import {
+  faFacebook,
+  faGooglePlus,
+  faTwitter,
+  faPinterestP,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 const ProductOverview = () => {
   return (
@@ -64,7 +78,44 @@ const ProductOverview = () => {
             </div>
           </div>
           <div className={`col-7`}>
-            <h5>name of product</h5>
+            <div className={styles.productHeader}>
+              <div className={styles.productName}>
+                <h5>name of product</h5>
+              </div>
+              <div className={styles.arrowButtons}>
+                <Button className={styles.headerButtonLeft} variant='outline'>
+                  <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+                </Button>
+                <Button className={styles.headerButtonRight} variant='outline'>
+                  <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+                </Button>
+              </div>
+            </div>
+            <div className={styles.reviews}>
+              <FontAwesomeIcon className={styles.star} icon={faStar}></FontAwesomeIcon>
+              <FontAwesomeIcon className={styles.star} icon={faStar}></FontAwesomeIcon>
+              <FontAwesomeIcon className={styles.star} icon={faStar}></FontAwesomeIcon>
+              <FontAwesomeIcon className={styles.star} icon={faStar}></FontAwesomeIcon>
+              <FontAwesomeIcon className={styles.star} icon={faStar}></FontAwesomeIcon>
+              <Link className={styles.reviewLink}>(0 reviews)</Link>
+              <div className={styles.verticalLine}>&#124;</div>
+              <Link className={styles.reviewLink}>Add Your Review</Link>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.prices}>
+              <Button className={styles.btnoldprice} variant='small'>
+                $350.00
+              </Button>
+              <Button className={styles.btnprice} noHover variant='small'>
+                $250.00
+              </Button>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.aboutProductButtons}></div>
+            <div className={styles.quantity}></div>
+            <div className={styles.quickOverview}></div>
+            <div className={styles.sorage}></div>
+            <div className={styles.socialMedia}></div>
           </div>
         </div>
       </div>
